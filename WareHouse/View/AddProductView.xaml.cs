8bbc797c0785c927063c41c6b39a85d097d3f891
Вова <blizzardVovan4ek@gmail.com>
2020-01-29 11:31:37 +0300

    Добавлена кнопка для открытия всплывающего окна, и само окно для добавления в бд.

---
+++ WareHouse/View/AddProductView.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WareHouse.Data;
+
+namespace WareHouse.View
+{
+    public partial class AddProductView : Window
+    {
+        public AddProductView()
+        {
+            InitializeComponent();
+        }
+        public Product NewProduct
+        {
+            get 
+            {
+                return new Product()
+                {
+                    CodeId = int.Parse(this.CodeId.Text),
+                    Name = this.Name.Text,
+                    Price = int.Parse(this.Price.Text)
+                };
+            }
+        }
+        private void Accept_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = true;
+        }
+
+        void OnPreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !e.Text.All(IsNumeric);
+        }
+
+        private void OnPasting(object sender, DataObjectPastingEventArgs e)
+        {
+            var stringData = (string)e.DataObject.GetData(typeof(string));
+            if (stringData == null || !stringData.All(IsNumeric))
+                e.CancelCommand();
+        }
+        private bool IsNumeric(char c)
+        {
+            return c >= '0' && c <= '9';
+        }
+    }
+}
---
+++ WareHouse/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Collections.ObjectModel;
+
+using WareHouse.Data;
+using WareHouse.Model;
+using System.Data.Entity;
+using WareHouse.View;
+using WareHouse.Helpers;
+
+namespace WareHouse.ViewModel
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        readonly DbInteractionModel model = new DbInteractionModel();
+
+        public ObservableCollection<Accept> Accepts => model.Accept;
+        public ObservableCollection<InStorage> InStorages => model.InStorages;
+        public ObservableCollection<Sale> Sales => model.Sales;
+        
+        private RelayCommand _addCommand;
+        public RelayCommand AddCommand
+        {
+            get
+            {
+                return _addCommand ??
+                    (_addCommand = new RelayCommand(_ =>
+                    {
+                        var addProduct = new AddProductView();
+
+                        if (addProduct.ShowDialog() == true)
+                        {
+                            model.TryAddToDb(addProduct.NewProduct);
+                        }
+                    }));
+            }
+        }
+        private RelayCommand _reportWindowCommand;
+        public RelayCommand ReportWindowCommand
+        {
+            get
+            {
+                return _reportWindowCommand ??
+                    (_reportWindowCommand = new RelayCommand(_ =>
+                    {
+                        ReportView reportView = new ReportView();
+                        reportView.Show();
+                    }));
+            }
+        }
+        private RelayCommand _sellCommand;
+        public RelayCommand SellCommand
+        {
+            get 
+            {
+                return _sellCommand ??
+                    (_sellCommand = new RelayCommand(a =>
+                    {
+                        model.SellProduct((InStorage)a);
+                    }));
+            }
+        }
+        public MainWindowViewModel()
+        {
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
